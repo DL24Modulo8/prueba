6e0afcbb1632851e0f968fea3dde8c78d3d8ce57
Francisco-ab7 <francisco.ab7@gmail.com>
2025-04-01 08:13:15 -0300

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -6,12 +6,12 @@ describe('API Tests', () => {
   it('should return a list of users', async () => {
     const res = await request(app).get('/users');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveLength(2);
+    expect(res.body).toHaveLength(4);
   });
 
   it('should return a single user', async () => {
     const res = await request(app).get('/users/1');
     expect(res.statusCode).toEqual(200);
-    expect(res.body.name).toEqual('Alice');
+    expect(res.body.name).toEqual('Blanca');
   });
 });
